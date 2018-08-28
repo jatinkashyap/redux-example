4df74d0e89b7bb642c0db10c3d3f36fd5dbf24c7
jatinkashyap <jatinkashyap9@gmail.com>
2018-08-28 10:30:24 -0400

    adding UI sate to redux

--- src/containers/Persons.js
+++ src/containers/Persons.js
@@ -50,7 +50,7 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        onAddPerson : () => dispatch({type : 'ADD_PERSON'}),
+        onAddPerson : (name,age) => dispatch({type : 'ADD_PERSON', personData : {name:name, age:age}}),
         onDeletePerson : (pId) => dispatch({type : 'DELETE_PERSON' , personId : pId}) 
     };
 };
